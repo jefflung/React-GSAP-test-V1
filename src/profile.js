7d944e033002e22907fb33dfd3377ca4235d1d76
Jeff Lung YK <nosy_parker@msn.com>
2018-03-21 11:09:56 +0800

    Add files via upload

---
+++ src/profile.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class profile extends Component {
+ render() {
+  return (
+   <div className="page_profile">
+    <h1>Profile</h1>
+    <p>Hello from the profile page!</p>
+   </div>
+  )
+ }
+}
